ebd8365fc0683aac878bed82dcaf9a010d8a02a7
Alex <amm1205@rit.edu>
2022-09-28 13:32:27 -0400

    Initial Commit

---
+++ Joke Generator/Joke_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Joke_GeneratorApp.swift
+//  Joke Generator
+//
+//  Created by Alex Murphy on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Joke_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
